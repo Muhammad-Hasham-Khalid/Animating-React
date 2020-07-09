bac974302bec57565f0ae00d01cd432758025b55
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-10 00:04:18 +0500

    Basics of react spring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Toggle } from "./Toggle";
+import { useSpring, animated } from "react-spring";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
+  return (
+    <>
+      <animated.div className="App" style={fade}>
         <header className="App-header">
           <img src={logo} className="logo" />
           <button className="menu-button">Menu</button>
         </header>
-      </div>
-    );
-  }
+        <main>
+          <Toggle />
+        </main>
+      </animated.div>
+    </>
+  );
 }
 
 export default App;
